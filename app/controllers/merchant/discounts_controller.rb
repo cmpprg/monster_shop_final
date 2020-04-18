0cf538cab0c003120fec9a3f75e4e430d45b63f5
Ryan Camp <campryan@comcast.net>
2020-04-18 14:40:15 -0600

    Implement functionality to create discount into merchant discount controller

--- app/controllers/merchant/discounts_controller.rb
+++ app/controllers/merchant/discounts_controller.rb
@@ -1,10 +1,20 @@
 class Merchant::DiscountsController < Merchant::BaseController
-  
+
   def index
 
   end
 
   def new
+    @discount = Discount.new
+  end
 
+  def create
+    @discount = current_user.merchant.discounts.create(discount_params)
+  end
+
+  private
+
+  def discount_params
+    params.require(:discount).permit(:min_quantity, :percentage)
   end
 end
