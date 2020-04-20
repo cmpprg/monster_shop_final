197a196ed6d331ba57e6b3adb5452451abd637ff
Ryan Camp <campryan@comcast.net>
2020-04-19 18:59:16 -0600

    Modify test to check for dynamic discount results

--- spec/features/merchant/discounts/cart_discount_spec.rb
+++ spec/features/merchant/discounts/cart_discount_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe "As a user, when I add enough quantity to cart." do
     @item1 = @merchant1.items.create!(name: 'Ogre', description: "I'm an Ogre!", price: 100, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 5 )
     @item2 = @merchant1.items.create!(name: 'Giant', description: "I'm a Giant!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
     @merchant_user = @merchant1.users.create(name: 'Megan', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218, email: 'megan@example.com', password: 'securepassword')
-    @discount1 = @merchant1.discounts.create!(min_quantity: 2, percentage: 20)
+    @discount1 = @merchant1.discounts.create!(min_quantity: 3, percentage: 20)
   end
 
   it "I see discount automatically show up on the cart page" do
@@ -17,12 +17,16 @@ RSpec.describe "As a user, when I add enough quantity to cart." do
     within("#item-#{@item1.id}") do
       expect(page).to have_content("Price: $100.00")
     end
-    within("#item-#{@item1.id}") do
-      click_button "More of This!"
+
+    2.times do
+      within("#item-#{@item1.id}") do
+        click_button "More of This!"
+      end
     end
+
     within("#item-#{@item1.id}") do
       expect(page).to have_content("Price: $80.00, 20% off")
-      expect(page).to have_content("Subtotal: $160.00")
+      expect(page).to have_content("Subtotal: $240.00")
     end
   end
 
