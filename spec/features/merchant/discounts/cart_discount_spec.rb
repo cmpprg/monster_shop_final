531c1121b31e196d05e442d84db46784b2614442
Ryan Camp <campryan@comcast.net>
2020-04-19 19:30:55 -0600

    Write test to ensure that highest discount is given to user in case of conflict

--- spec/features/merchant/discounts/cart_discount_spec.rb
+++ spec/features/merchant/discounts/cart_discount_spec.rb
@@ -10,6 +10,9 @@ RSpec.describe "As a user, when I add enough quantity to cart." do
     @item3 = @merchant2.items.create!(name: 'Unicorn', description: "I'm a Unicorn!", price: 20, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 10 )
     @merchant_user = @merchant1.users.create(name: 'Megan', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218, email: 'megan@example.com', password: 'securepassword')
     @discount1 = @merchant1.discounts.create!(min_quantity: 3, percentage: 20)
+    @discount1 = @merchant1.discounts.create!(min_quantity: 6, percentage: 30)
+    @discount1 = @merchant1.discounts.create!(min_quantity: 4, percentage: 40)
+
   end
 
   it "I see discount automatically show up on the cart page" do
@@ -101,9 +104,30 @@ RSpec.describe "As a user, when I add enough quantity to cart." do
       within("#item-#{@item2.id}") do
         expect(page).to have_content("Price: $50.00")
       end
-
-
   end
 
+  it "if there is conflict between two discounts, the greater of the two will be applied." do
+    @merchant1.discounts.create!(min_quantity: 6, percentage: 30)
+    @merchant1.discounts.create!(min_quantity: 4, percentage: 40)
 
+    visit item_path(@item1)
+    click_button("Add to Cart")
+
+    visit cart_path
+    within("#item-#{@item1.id}") do
+      expect(page).to have_content("Price: $100.00")
+    end
+
+    5.times do
+      within("#item-#{@item1.id}") do
+        click_button "More of This!"
+      end
+    end
+
+
+    within("#item-#{@item1.id}") do
+      expect(page).to have_content("Price: $60.00, 40% off")
+      expect(page).to have_content("Subtotal: $360.00")
+    end
+  end
 end
