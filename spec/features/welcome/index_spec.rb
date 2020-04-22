7f9b08a9c76ca39553ff9a5d6115115dfafc493a
Ryan Camp <campryan@comcast.net>
2020-04-21 20:13:16 -0600

    Write test to check for landing page

---
+++ spec/features/welcome/index_spec.rb
@@ -0,0 +1,9 @@
+require "rails_helper"
+
+RSpec.describe "As a visitor", type: :feature do
+  it "I see a welcome message and access to navbar on landing page." do
+    visit "/"
+
+    expect(page).to have_content("Welcome!")
+  end
+end
